6f084c8c41f3e4443c437b466a7caf5fec19b4d5
Kevin Jeff Ouano <ouanokevinj@gmail.com>
2026-06-22 12:50:39 +0800

    use SF Pro font for ios devices

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './index.css'
 import App from './App.vue'
 
+/* Apply SF Pro on all iOS browsers (Safari, Chrome, Firefox) */
+if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
+  document.documentElement.classList.add('ios')
+}
+
 createApp(App).mount('#app')
